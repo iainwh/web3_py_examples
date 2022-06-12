60239db196cf13f7596e33f14ab409987227e495
Iain <60200813+iainwh@users.noreply.github.com>
2022-06-12 10:43:54 +0100

    Update 1_accounts.py

--- examples/1_accounts.py
+++ examples/1_accounts.py
@@ -1,7 +1,7 @@
 from web3 import Web3
 
 # Fill in your infura API key here
-infura_url = "https://mainnet.infura.io/v3/YOUR_INFURA_API_KEY_GOES_HERE"
+infura_url = "https://mainnet.infura.io/v3/a724432af2964ead8717807bbc3a5656"
 web3 = Web3(Web3.HTTPProvider(infura_url))
 
 print(web3.isConnected())
@@ -9,5 +9,5 @@ print(web3.isConnected())
 print(web3.eth.blockNumber)
 
 # Fill in your account here
-balance = web3.eth.getBalance("YOUR_ACCOUNT_GOES_HERE")
+balance = web3.eth.getBalance("0x433393ee17085A1c4a03Cb23fB8559Ce227954F9")
 print(web3.fromWei(balance, "ether"))
